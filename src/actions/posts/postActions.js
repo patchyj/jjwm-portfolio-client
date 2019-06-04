4e810bb9e6e6dbc813eaea7c444aee495a3d6f79
patchyj <jackjwmcgregor@gmail.com>
2019-06-04 21:12:28 +0100

    testing

--- src/actions/posts/postActions.js
+++ src/actions/posts/postActions.js
@@ -8,6 +8,8 @@ import {
 import { getPostsQuery } from './postQueries';
 import { baseURL } from '../../utils/baseURL';
 
+// eslint-disable-next-line no-console
+console.log(process.env.NODE_ENV);
 export const getPosts = () => (dispatch) => {
   dispatch({ type: GET_POSTS_STARTED });
 
