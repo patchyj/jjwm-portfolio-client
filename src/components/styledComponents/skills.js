6e96beb9ab0408cca5d3f933acae6623761211eb
patchyj <jackjwmcgregor@gmail.com>
2019-11-16 13:35:29 +0000

    added more skills

--- src/components/styledComponents/skills.js
+++ src/components/styledComponents/skills.js
@@ -4,10 +4,22 @@ import styled from 'styled-components';
 export const SkillsWrapper = styled.div`
   min-height: 100vh;
   padding: 1rem;
-  box-sizing:border-box;
+  box-sizing: border-box;
+
+  .row {
+    align-items: center;
+
+    label {
+      display: inline;
+    }
+  }
+
+  .progress {
+    height: 0.5rem;
+  }
 `;
 
 export const StyledProgressBar = styled.div`
   border-radius: 20px;
-  background: linear-gradient(45deg, #e66465, #9198e5);
+  background: linear-gradient(85deg, #4caf50, #673ab7);
 `;
