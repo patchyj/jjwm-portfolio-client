1dca9ea01f31498de4e3a867b66fb39680d22718
patchyj <jackjwmcgregor@gmail.com>
2019-11-20 19:38:59 +0000

    adding navbar breaks

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from '../styledComponents';
+import { dimensions } from '../styledComponents/breakpoints';
 import NavLink from './NavLink';
 
-const Navbar = () => (
-  <S.Nav>
-    <nav className="navbar navbar-expand fixed-top flex-md-column flex-row align-items-start">
-      <div className="collapse navbar-collapse">
-        <ul className="flex-md-column flex-row navbar-nav w-100 justify-content-sm-around">
-          <NavLink title="Home" />
-          <NavLink page="about" title="About" />
-          <NavLink page="portfolio" title="Portfolio" />
-          <NavLink page="skills" title="Skills" />
-          <NavLink page="experience" title="Experience" />
-        </ul>
-      </div>
-    </nav>
-  </S.Nav>
-);
+const setCollapse = () => window.innerWidth < dimensions.bsSmall;
+
+const Navbar = () => {
+  // console.log(setCollapse());
+  const [responsive, setNavbar] = useState(setCollapse());
+  console.log(responsive);
+  return (
+    <S.Nav>
+      <nav className="navbar navbar-expand fixed-top flex-md-column flex-row align-items-start">
+        <div className="collapse navbar-collapse" id="nav">
+          <ul className="flex-md-column flex-row navbar-nav w-100 justify-content-sm-around">
+            <NavLink title="Home" />
+            <NavLink page="about" title="About" />
+            <NavLink page="portfolio" title="Portfolio" />
+            <NavLink page="skills" title="Skills" />
+            <NavLink page="experience" title="Experience" />
+          </ul>
+          <button className="navbar-toggler" type="button" onClick={setNavbar}>
+            <i className="fas fa-bars" />
+          </button>
+        </div>
+      </nav>
+    </S.Nav>
+  );
+};
 
 export default Navbar;
--- src/components/styledComponents/breakpoints.js
+++ src/components/styledComponents/breakpoints.js
@@ -1,12 +1,23 @@
 // When importing, call this file 'maxWidth'
 
+export const dimensions = {
+  extraSmall: 325,
+  small: 540,
+  bsSmall: 575,
+  medium: 720,
+  bsMedium: 768,
+  large: 960,
+  bsLarge: 991,
+  extraLarge: 1140,
+}
+
 export default {
-  extraSmall: '325px',
-  small: '540px',
-  bsSmall: '575px',
-  medium: '720px',
-  bsMedium: '768px',
-  large: '960px',
-  bsLarge: '991px',
-  extraLarge: '1140px',
+  extraSmall: `${dimensions.extraSmall}px`,
+  small: `${dimensions.small}px`,
+  bsSmall: `${dimensions.bsSmall}px`,
+  medium: `${dimensions.medium}px`,
+  bsMedium: `${dimensions.bsMedium}px`,
+  large: `${dimensions.large}px`,
+  bsLarge: `${dimensions.bsLarge}px`,
+  extraLarge: `${dimensions.extraLarge}px`,
 };
