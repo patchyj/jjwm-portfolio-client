7b4216b533e15c0e98837de4c1194117073b6e72
patchyj <jackjwmcgregor@gmail.com>
2019-11-22 13:17:31 +0000

    full screen nav at small

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,29 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/interactive-supports-focus */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import S from '../styledComponents';
 import { dimensions } from '../styledComponents/breakpoints';
 import NavLink from './NavLink';
 
 const shouldCollapse = (width, breakpoint) => width < breakpoint;
-// const shouldCollapse = (width, break) => width < dimensions.bsSmall;
 
 const Navbar = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [responsiveMode, setResponsiveMode] = useState(false);
   const [isOpen, setOpen] = useState(false);
+  const overFlow = isOpen ? 'hidden' : 'auto';
+
+  document.body.style.overflowY = overFlow;
 
   if (shouldCollapse(windowWidth, dimensions.bsSmall) && !responsiveMode) {
-    console.log('BREAK');
     setResponsiveMode(true);
   }
 
   if (!shouldCollapse(windowWidth, dimensions.bsSmall) && responsiveMode) {
-    console.log('UNBREAK');
-    setResponsiveMode(false);
+    setOpen(false);
+    if (responsiveMode) {
+      setResponsiveMode(false);
+    }
   }
 
   const resizeWindow = () => {
@@ -33,15 +36,26 @@ const Navbar = () => {
     return () => window.removeEventListener('resize', resizeWindow);
   }, []);
 
-  console.log(isOpen);
-
   return (
-    <S.Nav>
+    <S.Nav isOpen={isOpen}>
       <nav className="navbar navbar-expand fixed-top flex-md-column flex-row align-items-start">
         <div className="collapse navbar-collapse p-2" id="nav">
-
           {responsiveMode ? (
-            <i className="fas fa-bars fa-3x" role="button" onClick={() => setOpen(!isOpen)} />
+            <div className="">
+              <i
+                className={`fas fa-${isOpen ? 'times fadeIn' : 'bars'} fa-2x`}
+                role="button"
+                onClick={() => setOpen(!isOpen)}
+              />
+              <br />
+              {isOpen && (
+                <div className={`dropdown ${isOpen ? 'fadeIn' : ''}`}>
+                  <ul className=" ">
+                    <li>blah</li>
+                  </ul>
+                </div>
+              )}
+            </div>
           ) : (
             <ul className="flex-md-column flex-row navbar-nav w-100 justify-content-sm-around">
               <NavLink title="Home" />
@@ -51,7 +65,6 @@ const Navbar = () => {
               <NavLink page="experience" title="Experience" />
             </ul>
           )}
-
         </div>
       </nav>
     </S.Nav>
--- src/components/styledComponents/nav.js
+++ src/components/styledComponents/nav.js
@@ -1,8 +1,13 @@
 /* eslint-disable import/prefer-default-export */
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { fadeIn } from 'react-animations';
 import maxWidth from './breakpoints';
 
+const fadeInAnimation = keyframes`${fadeIn}`;
+
 export const Nav = styled.div`
+  overflow-y: ${({ isOpen }) => (isOpen ? 'hidden' : 'auto')};
+
   .navbar {
     padding: 0;
     height: 100vh;
@@ -16,8 +21,26 @@ export const Nav = styled.div`
   .fixed-top {
     z-index: 0;
   }
-  .fa-bars {
+  .fa-bars,
+  .fa-times {
     cursor: pointer;
+  }
+
+  .fadeIn {
+    animation: 1s ${fadeInAnimation};
+  }
+
+  .dropdown {
+    display: flex;
+    flex-direction: column;
+
+    ul {
+      padding: 0;
+      list-style: none;
+
+      li {
+      }
+    }
   }
 
   @media (max-width: ${maxWidth.small}) {
@@ -28,7 +51,8 @@ export const Nav = styled.div`
 
   @media (max-width: ${maxWidth.medium}) {
     .navbar {
-      height: auto;
+      height: ${({ isOpen }) => (isOpen ? '100vh' : 'auto')};
+      background: rgb(231, 245, 245);
     }
 
     .fixed-top {
